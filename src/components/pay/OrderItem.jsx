eb9172ab162bd753a4ce1a9a15ff1eed6fd5e307
dobrqka <dobrin.andreev@gmail.com>
2024-04-23 12:43:37 +0300

    Calculator styles

--- src/components/pay/OrderItem.jsx
+++ src/components/pay/OrderItem.jsx
@@ -7,14 +7,14 @@ export const OrderItem = ({ title, price, quantity, remove, image }) => {
         <img src={image}></img>
       </div>
       <h3>{title}</h3>
-      <div className="remove-container">
-        <button className="remove-button" onClick={remove}>
-          X
-        </button>
-      </div>
       <p>${price.toFixed(2)}</p>
       <p>x{quantity}</p>
       <p>Price: ${(price * quantity).toFixed(2)}</p>
+      <div className="remove-container">
+        <button className="remove-button" onClick={remove}>
+          x
+        </button>
+      </div>
     </div>
   );
 };
