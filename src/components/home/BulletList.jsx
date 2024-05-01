7f949cbf21e6d46a5340ae6c7607e6347041b364
dobrqka <dobrin.andreev@gmail.com>
2024-05-01 07:52:55 +0300

    Set tablet design on home page

--- src/components/home/BulletList.jsx
+++ src/components/home/BulletList.jsx
@@ -3,11 +3,11 @@ import "../../styles/home/BulletList.css";
 
 export const BulletList = () => {
   const para1 =
-    "We prioritize soil health and biodiversity and this creates healthy plants without the need for any chemicals!";
+    "We prioritize soil health, fertility and biodiversity, and avoid chemicals of all sorts which leads to healthy and nutritious plants!";
   const para2 =
     "We pick the vegetables as soon as we get your order and they are shipped within the hour.";
   const para3 =
-    "Our careful selection of varieties (based on nutritional value and taste) coupled with clean, organic growing (with love!) guarantee a yummy meal!";
+    "Our careful selection of varieties based on nutritional value and taste coupled with clean, organic growing (with love!) guarantee a yummy meal!";
 
   return (
     <div className="bullet-list">
